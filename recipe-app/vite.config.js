312037a70e4521149002b6f75551753de9e3a5ca
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2023-10-09 18:32:49 +0100

    chore: Change base from full url to absolute url pathname

--- recipe-app/vite.config.js
+++ recipe-app/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://chingu-voyages.github.io/v46-tier2-team-17",
+  base: "/v46-tier2-team-17/",
 });
