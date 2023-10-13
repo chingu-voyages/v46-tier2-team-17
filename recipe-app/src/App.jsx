dbfbd11189db8dd4224f57e1552e51069ae65248
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2023-10-13 21:37:16 +0100

    feat: design the homepage's layout

--- recipe-app/src/App.jsx
+++ recipe-app/src/App.jsx
@@ -1,8 +1,29 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="page">
+        <nav className="page__nav">
+          <a href="/">App Name</a>
+          <button>Menu</button>
+        </nav>
+        <aside className="page__aside"></aside>
+        <main className="page__gallery">
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+        </main>
+      </div>
+    </>
+  );
 }
 
 export default App;
