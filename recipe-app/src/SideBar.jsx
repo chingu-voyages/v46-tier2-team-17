4e4535fb1804c3e9b22fb7a86cefe53944187c4d
Omonigho Ovie Oddiri <ovie52009@yahoo.com>
2023-10-15 13:55:15 +0100

    feat(app): added sidebar

--- recipe-app/src/SideBar.jsx
+++ recipe-app/src/SideBar.jsx
@@ -28,7 +28,7 @@ export default function SideBar() {
           <p>dairy</p>
           <p>vegan</p>
           <p>pescatarian</p>
-          <p>low carb diet</p>
+          <p>low carb</p>
         </div>
       </div>
     </main>
