03c2e617fb99242d7da2de8a28d039f9534a08e5
Omonigho Ovie Oddiri <ovie52009@yahoo.com>
2023-10-19 13:58:38 +0100

    Merge branch 'development' of https://github.com/chingu-voyages/v46-tier2-team-17 into sidebar

--- recipe-app/src/App.jsx
+++ recipe-app/src/App.jsx
@@ -1,14 +1,74 @@
+
+import SideBar from "./SideBar";
+
+
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import SideBar from "./SideBar";
-// import "./SideBar.css"
+import Card from "./components/Card";
+
 
 function App() {
   return (
     <>
+
       <SideBar />
+
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+
+      <div className="page">
+        <nav className="page__nav">
+          <a className="page__nav-logo" href="/">
+            App Name
+          </a>
+          <button className="page__nav-menu">Menu</button>
+        </nav>
+        <aside className="page__aside"></aside>
+        <main className="page__gallery">
+          <div className="card">
+            <Card />
+          </div>
+          <div className="card">
+            <Card />
+          </div>
+          <div className="card">
+            <Card />
+          </div>
+          <div className="card">
+            <Card />
+          </div>
+          <div className="card">
+            <Card />
+          </div>
+          <div className="card">
+            <Card />
+          </div>
+          <div className="card">
+            <Card />
+          </div>
+          <div className="card">
+            <Card />
+          </div>
+          <div className="card">
+            <Card />
+          </div>
+          <div className="card">
+            <Card />
+          </div>
+        </main>
+      </div>
+
     </>
   );
 }
---
+++ recipe-app/src/components/Card.jsx
@@ -0,0 +1,14 @@
+const Card = () => {
+  return (
+    <div className="recipes-card">
+      <div className="image-container">
+        <img src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?resize=768,574" />
+      </div>
+      <div className="content">
+        <h2>Chorizo & mozzarella gnocchi bake</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
