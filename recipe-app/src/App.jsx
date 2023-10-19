5a71843250073176a443ce5c0e58bef57b30515d
Omonigho Ovie Oddiri <ovie52009@yahoo.com>
2023-10-19 14:02:38 +0100

    feat(sidebar): commiting changes made to sidebar component

--- recipe-app/src/App.jsx
+++ recipe-app/src/App.jsx
@@ -1,10 +1,5 @@
 
 import SideBar from "./SideBar";
-
-
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Card from "./components/Card";
 
@@ -12,21 +7,6 @@ import Card from "./components/Card";
 function App() {
   return (
     <>
-
-      <SideBar />
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-
       <div className="page">
         <nav className="page__nav">
           <a className="page__nav-logo" href="/">
@@ -34,7 +14,9 @@ function App() {
           </a>
           <button className="page__nav-menu">Menu</button>
         </nav>
-        <aside className="page__aside"></aside>
+        <aside className="page__aside">
+          <SideBar />
+        </aside>
         <main className="page__gallery">
           <div className="card">
             <Card />
@@ -68,7 +50,6 @@ function App() {
           </div>
         </main>
       </div>
-
     </>
   );
 }
