06b6eec4220420ad786d62acedc4e85ed4487d0c
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2023-10-21 01:46:37 +0100

    feat: Design welcome component

--- recipe-app/src/components/Welcome.jsx
+++ recipe-app/src/components/Welcome.jsx
@@ -1,9 +1,20 @@
+import recipes from "../data/recipes";
+
 function Welcome() {
+  const selectedRecipeDataObj =
+    recipes.results[Math.floor(Math.random() * recipes.results.length)];
+  const selectedRecipeImage = selectedRecipeDataObj.thumbnail_url;
+
   return (
-    <article className="welcome-component">
-      <h2 className="welcome-component__heading">
-        Find the best recipes for your ingredients!
-      </h2>
+    <article
+      className="welcome-component"
+      style={{ backgroundImage: `url(${selectedRecipeImage})` }}
+    >
+      <div className="welcome-component__overlay">
+        <h2 className="welcome-component__heading">
+          Find the best recipes for your ingredients!
+        </h2>
+      </div>
     </article>
   );
 }
