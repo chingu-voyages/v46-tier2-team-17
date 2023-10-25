f7f470ce9b789af3f9e1015f83eb1cbad98b1035
Kara <karaguarraci@gmail.com>
2023-10-25 19:15:24 +0100

    fix:fixed propType error on card

--- recipe-app/src/components/Card.jsx
+++ recipe-app/src/components/Card.jsx
@@ -1,11 +1,20 @@
+import PropTypes from "prop-types";
+
 const Card = ({ result }) => {
+  const { name, thumbnail_url } = result;
+  Card.propTypes = {
+    result: PropTypes.shape({
+      thumbnail_url: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+    }),
+  };
   return (
     <div
       className="recipes-card"
-      style={{ backgroundImage: `url(${result.thumbnail_url})` }}
+      style={{ backgroundImage: `url(${thumbnail_url})` }}
     >
       <div className="content">
-        <h2>{result.name}</h2>
+        <h2>{name}</h2>
       </div>
     </div>
   );
