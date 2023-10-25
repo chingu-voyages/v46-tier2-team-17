2003c0c285b530eec69417eb244d7d37b49494b0
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2023-10-25 18:32:46 +0100

    style: Resolve card styling issues

--- recipe-app/src/components/Card.jsx
+++ recipe-app/src/components/Card.jsx
@@ -1,9 +1,9 @@
 const Card = ({ result }) => {
   return (
-    <div className="recipes-card">
-      <div className="image-container">
-        <img src={result.thumbnail_url} alt={result.name} />
-      </div>
+    <div
+      className="recipes-card"
+      style={{ backgroundImage: `url(${result.thumbnail_url})` }}
+    >
       <div className="content">
         <h2>{result.name}</h2>
       </div>
