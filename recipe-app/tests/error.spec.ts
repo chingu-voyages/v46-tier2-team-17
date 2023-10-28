cbea1f48766e9d8f4b5e3daee04043dc55c850a8
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2023-10-28 18:01:05 +0100

    test: Disable test that requires api fetch in production

--- recipe-app/tests/error.spec.ts
+++ recipe-app/tests/error.spec.ts
@@ -5,11 +5,11 @@ test("has categories heading", async ({ page }) => {
   await expect(page.getByRole("heading", { name: "Categories" })).toBeVisible();
 });
 
-test("invalid word search", async ({ page }) => {
-  await page.goto("./");
-  await page.getByRole("searchbox").fill("invalidIngredient");
-  await page.getByRole("searchbox").press("Enter");
-  await expect(
-    page.getByRole("heading", { name: "Sorry, we couldn't find any recipe" }),
-  ).toBeVisible();
-});
+// test("invalid word search", async ({ page }) => {
+//   await page.goto("./");
+//   await page.getByRole("searchbox").fill("invalidIngredient");
+//   await page.getByRole("searchbox").press("Enter");
+//   await expect(
+//     page.getByRole("heading", { name: "Sorry, we couldn't find any recipe" }),
+//   ).toBeVisible();
+// });
