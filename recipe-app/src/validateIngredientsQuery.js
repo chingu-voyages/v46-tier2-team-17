c1c173719a7c6f52bf00743f4e5e3870bc82b892
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2023-10-28 13:47:37 +0100

    refactor: Remove tags parameter from url

--- recipe-app/src/validateIngredientsQuery.js
+++ recipe-app/src/validateIngredientsQuery.js
@@ -1,8 +1,7 @@
 function validateIngredientsQuery(searchedText) {
   if (/^[^_|\W]/.test(searchedText)) {
     const errorModal = document.getElementById("error-modal");
-    const url =
-      "https://tasty.p.rapidapi.com/recipes/list?from=0&size=1&tags=under_30_minutes";
+    const url = "https://tasty.p.rapidapi.com/recipes/list?from=0&size=20";
     const options = {
       method: "GET",
       headers: {
@@ -10,9 +9,7 @@ function validateIngredientsQuery(searchedText) {
         "X-RapidAPI-Host": "tasty.p.rapidapi.com",
       },
     };
-
     errorModal.style.display = "none";
-
     async function checkIfRecipeExistsForIngredients() {
       try {
         let searchedWord = null;
@@ -27,36 +24,26 @@ function validateIngredientsQuery(searchedText) {
             .trim()
             .split(/[\W|_]/g)
             .filter((i) => i);
-
-          console.log(searchedWordsArray);
-
           return searchedWordsArray.some((w) => {
             searchedWord = w;
             return recipesArray.some((r) => {
               return r.sections[0].components.some((c) => {
-                console.log(c.ingredient.name);
                 const regExpVersionOfSearchedWord = new RegExp(w);
                 return regExpVersionOfSearchedWord.test(c.ingredient.name);
               });
             });
           });
         }
-
-        console.log(hasSearchedIngredients);
         if (!hasSearchedIngredients) {
           const ingredient404Element =
             document.getElementById("ingredient-404");
-
           ingredient404Element.innerText = searchedWord;
           errorModal.style.display = "flex";
-          console.log(searchedWord);
         }
-        console.log(recipesArray);
       } catch (error) {
         console.error(error);
       }
     }
-    console.log(searchedText);
     checkIfRecipeExistsForIngredients();
   }
 }
