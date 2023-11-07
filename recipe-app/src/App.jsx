07b33baa8205ef32d1080771d35ab81463c68b70
Kara <karaguarraci@gmail.com>
2023-11-07 22:39:54 +0000

    feat: create loader component

--- recipe-app/src/App.jsx
+++ recipe-app/src/App.jsx
@@ -4,16 +4,18 @@ import SideBar from "./components/SideBar";
 import Welcome from "./components/Welcome";
 import Card from "./components/Card";
 import Error from "./components/Error";
+import Loader from "./components/Loader";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [gallery, setGallery] = useState(true);
+  // const [gallery, setGallery] = useState(true);
   const [modal, setModal] = useState(false);
   const [chosenRecipe, SetChosenRecipe] = useState(null);
   const [allRecipes, setAllRecipes] = useState([]);
 
   // Pass setAllRecipes state to the Sidebar compponent
   // useEffect(() => {
+
   //   setAllRecipes(data.results);
   // }, []);
 
@@ -27,14 +29,15 @@ function App() {
           "X-RapidAPI-Host": "tasty.p.rapidapi.com",
         },
       };
-
+      const loaderModal = document.getElementById("loader-modal");
+      loaderModal.style.display = "flex";
       try {
         const response = await fetch(url, options);
         const result = await response.text();
         const recipesArray = JSON.parse(result).results;
-
         // console.log(recipesArray);
         setAllRecipes(recipesArray);
+        loaderModal.style.display = "none";
       } catch (error) {
         console.error(error);
       }
@@ -61,34 +64,24 @@ function App() {
         <SideBar
           setAllRecipes={(recipesArray) => setAllRecipes(recipesArray)}
         />
+        <div className="gallery">
+          <Welcome />
+          <Error
+            setAllRecipes={(recipesArray) => setAllRecipes(recipesArray)}
+          />
 
-        <Welcome />
-        <Error />
-        {gallery === true && (
-          <div className="gallery">
-            {allRecipes.map((result) => (
-              <Card
-                key={result.id}
-                result={result}
-                onClick={() => showModal(result)}
-              />
-            ))}
-          </div>
-        )}
+          <Loader />
+
+          {allRecipes.map((result) => (
+            <Card
+              key={result.id}
+              result={result}
+              onClick={() => showModal(result)}
+            />
+          ))}
+        </div>
         {modal === true && <Modal recipe={chosenRecipe} onClose={hideModal} />}
-
-        {/*
-          
-      */}
       </main>
-      <footer>
-        <a
-          href="https://github.com/chingu-voyages/v46-tier2-team-17"
-          target="_blank"
-        >
-          v46-Tier2-Team17 GitHub Repo
-        </a>
-      </footer>
     </>
   );
 }
---
+++ recipe-app/src/components/Loader.jsx
@@ -0,0 +1,9 @@
+function Loader() {
+  return (
+    <article id="loader-modal" className="loader-modal">
+      <p>Loading....</p>
+    </article>
+  );
+}
+
+export default Loader;
