6c804c8f0a156a7be2d54a4b294c28c808d66311
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2023-11-10 02:13:33 +0100

    chore: Uncomment the api fetching code

--- recipe-app/src/components/Gallery.jsx
+++ recipe-app/src/components/Gallery.jsx
@@ -5,7 +5,7 @@ import Error from "./Error";
 import Loader from "./Loader";
 import RecipeModal from "./RecipeModal";
 import Welcome from "./Welcome";
-import recipes from "../recipesListObj";
+// import recipes from "../recipesListObj";
 
 function Gallery({
   recipeModal,
@@ -17,34 +17,34 @@ function Gallery({
   searchedIngredients,
   setSearchedIngredients,
 }) {
-  useEffect(() => {
-    setAllRecipes(recipes.results);
-  }, []);
-
   // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const url = "https://tasty.p.rapidapi.com/recipes/list?from=0&size=20";
-  //     const options = {
-  //       method: "GET",
-  //       headers: {
-  //         "X-RapidAPI-Key": import.meta.env.VITE_X_RAPIDAPI_KEY,
-  //         "X-RapidAPI-Host": "tasty.p.rapidapi.com",
-  //       },
-  //     };
-  //     const loaderModal = document.getElementById("loader-modal");
-  //     loaderModal.style.display = "flex";
-  //     try {
-  //       const response = await fetch(url, options);
-  //       const result = await response.text();
-  //       const recipesArray = JSON.parse(result).results;
-  //       setAllRecipes(recipesArray);
-  //       loaderModal.style.display = "none";
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   };
-  //   fetchData();
+  //   setAllRecipes(recipes.results);
   // }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const url = "https://tasty.p.rapidapi.com/recipes/list?from=0&size=20";
+      const options = {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key": import.meta.env.VITE_X_RAPIDAPI_KEY,
+          "X-RapidAPI-Host": "tasty.p.rapidapi.com",
+        },
+      };
+      const loaderModal = document.getElementById("loader-modal");
+      loaderModal.style.display = "flex";
+      try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        const recipesArray = JSON.parse(result).results;
+        setAllRecipes(recipesArray);
+        loaderModal.style.display = "none";
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
 
   // Create ingredient <span>s from user's query
   function createSearchedIngredientsTags(searchedIngredients) {
